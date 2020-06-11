0247f8811999a116dee43989966e8b1acc01aa43
Yage Hu <yagehu@qq.com>
2020-06-10 17:19:05 -0700

    Add signal handling

--- internal/routes/register.go
+++ internal/routes/register.go
@@ -3,6 +3,8 @@ package routes
 import (
 	"context"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"go.uber.org/fx"
 	"go.uber.org/zap"
@@ -15,9 +17,10 @@ import (
 type Params struct {
 	fx.In
 
-	Logger    *zap.Logger
-	Lifecycle fx.Lifecycle
-	Handler   hello.Handler
+	Logger     *zap.Logger
+	Lifecycle  fx.Lifecycle
+	Handler    hello.Handler
+	Shutdowner fx.Shutdowner
 }
 
 // Register registers the routes for the server and starts the server on app
@@ -35,6 +38,25 @@ func Register(p Params) {
 			OnStart: func(context.Context) error {
 				p.Logger.Info("Starting server.")
 				go server.ListenAndServe()
+
+				c := make(chan os.Signal, 1)
+				signal.Notify(c, os.Interrupt)
+
+				// Block until a signal is received.
+				go func() {
+					s := <-c
+
+					p.Logger.Info(
+						"Got signal.",
+						zap.String("signal", s.String()),
+					)
+
+					if err := p.Shutdowner.Shutdown(); err != nil {
+						p.Logger.Error("Could not shutdown.", zap.Error(err))
+						os.Exit(1)
+					}
+				}()
+
 				return nil
 			},
 			OnStop: func(ctx context.Context) error {
